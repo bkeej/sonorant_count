a34aa38a0efe4e4ee278b6202daa77fe4646d3ff
Robert Henderson <balamkej@gmail.com>
2020-07-22 11:07:40 -0700

    get_words func

--- sonorant_count.py
+++ sonorant_count.py
@@ -1,2 +1,18 @@
-data_folder = '../../Recordings/2019_narratives/Completed_transcriptions/'
+import os
+import csv 
+import xml.etree.ElementTree as ET 
 
+narrative_dir = '../../Recordings/2019_narratives/Completed_transcriptions/'
+
+palmer_dir = '../special_eureka/uspanteko_corpus_xml/'
+
+def get_words(xmlfile):
+	tree = ET.parse(corpus_dir + xmlfile)
+	root = tree.getroot()
+
+	word_list = []
+
+	for phrase in root.findall('./body/phrases/phrase'):
+		word_list = word_list + phrase.findall('word')
+	
+	return word_lis
