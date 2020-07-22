0d2b4b5f54133431c3be2c76e41e48a482302051
Robert Henderson <balamkej@gmail.com>
2020-07-22 11:45:20 -0700

    minimal working examples for eaf and igtxml files

--- sonorant_count.py
+++ sonorant_count.py
@@ -4,27 +4,46 @@ import xml.etree.ElementTree as ET
 
 narrative_dir = '../../Recordings/2019_narratives/Completed_transcriptions/'
 
-palmer_dir = '../special_eureka/uspanteko_corpus_xml/'
+palmer_dir = '../special-eureka/data/uspanteko_corpus_xml/'
 
 def igtxml_get_words(dir,xmlfile):
-	tree = ET.parse(dir + xmlfile)
-	root = tree.getroot()
+	with open(dir + xmlfile) as f:
+		tree = ET.parse(f)
+		root = tree.getroot()
 
-	word_list = []
+		word_list = []
 
-	for phrase in root.findall('./body/phrases/phrase'):
-		word_list = word_list + phrase.findall('word')
-	
+		for phrase in root.findall('./body/phrases/phrase'):
+			word_list = word_list + phrase.findall('word')
+
+		word_list = [x.get('text') for x in word_list]
+
+		word_list = [''.join(list(filter(lambda ch: ch not in " :+.!¡¿?-,", x))) for x in word_list]
+
 	return word_list
 
 def elan_get_words(dir,eaffile):
-	tree = ET.parse(dir + eaffile)
-	root = tree.getroot()
+	with open(dir + eaffile) as f:
+		tree = ET.parse(f)
+		root = tree.getroot()
 
-	word_list = []
+		word_list = []
 
-	for phrase in root.findall("./TIER[@TIER_ID='Transcripción']/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE"):
-		for word in phrase.split():
-			word_list.append(word)
+		for phrase in root.findall("./TIER[@TIER_ID='Transcripción']/ANNOTATION/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE"):
+			for word in phrase.text.split():
+				word_list.append(word)
 
 	return word_list
+
+eaffile = "SPK_TAML_2020_Loq'laj teew pach loq'laj q'iij - El viento del norte y el sol.eaf"
+xmlfile = "1.xml"
+
+def main():
+
+	print(elan_get_words(narrative_dir,eaffile))
+
+	print(igtxml_get_words(palmer_dir,xmlfile))
+
+if __name__ == '__main__':
+	main() 
+
