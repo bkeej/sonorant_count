b702d51f5030c4f27ae81f934edc4517db7259e8
Robert Henderson <balamkej@gmail.com>
2020-07-22 11:20:29 -0700

    elan_get_words func

--- sonorant_count.py
+++ sonorant_count.py
@@ -6,7 +6,7 @@ narrative_dir = '../../Recordings/2019_narratives/Completed_transcriptions/'
 
 palmer_dir = '../special_eureka/uspanteko_corpus_xml/'
 
-def get_words(dir,xmlfile):
+def igtxml_get_words(dir,xmlfile):
 	tree = ET.parse(dir + xmlfile)
 	root = tree.getroot()
 
@@ -15,4 +15,16 @@ def get_words(dir,xmlfile):
 	for phrase in root.findall('./body/phrases/phrase'):
 		word_list = word_list + phrase.findall('word')
 	
+	return word_list
+
+def elan_get_words(dir,eaffile):
+	tree = ET.parse(dir + eaffile)
+	root = tree.getroot()
+
+	word_list = []
+
+	for phrase in root.findall("./TIER[@TIER_ID='Transcripción']/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE"):
+		for word in phrase.split():
+			word_list.append(word)
+
 	return word_list
