1b6553f21ed097ff8b47caa4f7a3861f36afdf95
Robert Henderson <balamkej@gmail.com>
2020-07-23 08:22:01 -0700

    word freq extracted

--- word_freq.py
+++ word_freq.py
@@ -2,7 +2,8 @@ import os
 import csv 
 from collections import defaultdict
 
-corpus = 'uspanteko_words.txt'
+corpus = 'uspanteko_words_just_B+H.txt'
+outfile = 'usp_word_freq_just_B+H.csv'
 
 def def_value(): 
     return False
@@ -11,4 +12,12 @@ d = defaultdict(def_value)
 
 with open(corpus, "r") as infile:
 	for w in infile:
-		w.strip()
+		w = w.strip()
+		if d[w]:
+			d[w] = d[w] + 1
+		else:
+			d[w] = 1
+
+with open(outfile, "w") as f:
+	wri = csv.writer(f)
+	wri.writerows(d.items())
