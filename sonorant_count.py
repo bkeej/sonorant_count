c82b57107775d5977ac39c390df0888833b3b477
Robert Henderson <balamkej@gmail.com>
2020-07-23 13:09:18 -0700

    sonorant count script finished + output data

--- sonorant_count.py
+++ sonorant_count.py
@@ -1,4 +1,36 @@
 import os
 import csv 
 
-sonorants = ['l', 'm', 'n', 'w', 'y', 'r']
+sonorants = 'lmnwyr'
+vowels = 'ieaou'
+
+csvcorpus = 'usp_word_freq.csv'
+corpus = 'uspanteko_words.txt'
+
+counts = []
+
+with open(csvcorpus) as f:
+	word_list = [row.split()[0] for row in f]
+	for w in word_list:
+		w = w.strip()
+		counts.append([len(w), len(''.join([''.join(list(filter(lambda ch: ch in sonorants, x))) for x in w])), len(''.join([''.join(list(filter(lambda ch: ch not in vowels, x))) for x in w]))])
+
+print('\nAverage sonorants per unique word:', sum([x[1] for x in counts]) / len(counts), '\n')
+
+print('Average characters per unique word:', sum([x[0] for x in counts]) / len(counts), '\n')
+
+print('Average non-vowel characters per unique word:', sum([x[2] for x in counts]) / len(counts), '\n')
+
+print('-----------------\n')
+
+with open(corpus) as f:
+	for w in f:
+		w = w.strip()
+		counts.append([len(w), len(''.join([''.join(list(filter(lambda ch: ch in sonorants, x))) for x in w])), len(''.join([''.join(list(filter(lambda ch: ch not in vowels, x))) for x in w]))])
+
+print('Average sonorants per word:', sum([x[1] for x in counts]) / len(counts), '\n')
+
+print('Average characters per word:', sum([x[0] for x in counts]) / len(counts), '\n')
+
+print('Average non-vowel characters per word:', sum([x[2] for x in counts]) / len(counts), '\n')
+
