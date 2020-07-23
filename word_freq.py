49a7abe088bf29bb93fb32be8fb316912b4ddb2d
Robert Henderson <balamkej@gmail.com>
2020-07-23 07:48:55 -0700

    default dict and value

--- word_freq.py
+++ word_freq.py
@@ -1,2 +1,14 @@
 import os
 import csv 
+from collections import defaultdict
+
+corpus = 'uspanteko_words.txt'
+
+def def_value(): 
+    return False
+
+d = defaultdict(def_value)
+
+with open(corpus, "r") as infile:
+	for w in infile:
+		w.strip()
