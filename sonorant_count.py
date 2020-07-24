ae94efb3479bab3205c0cc35bbd233c043ae1255
Robert Henderson <balamkej@gmail.com>
2020-07-24 11:49:22 -0700

    integrated grapheme correction for frequency corpus

--- sonorant_count.py
+++ sonorant_count.py
@@ -10,8 +10,9 @@ from uspanteko_g2p import *
 # Bigraphs: [ b' t' k' q' tz ch ]
 # Trigraphs: [ tz' ch' ]
 
-sonorants = 'lmnwyr'
-vowels = 'ieaou'
+sonorants = 'lmnwyɾ'
+vowels = 'ieaouiːeːaːoːuː'
+obstruents = 'ɓtʼkʼqʼt͡st͡ʃptkqsʃxʔt͡sʼt͡ʃʼ'
 
 csvcorpus = 'usp_word_freq.csv'
 corpus = 'uspanteko_words.txt'
@@ -19,12 +20,22 @@ corpus = 'uspanteko_words.txt'
 counts = []
 
 with open(csvcorpus) as f:
-	word_list = [row.split()[0] for row in f]
+	word_list = [row.split(',')[0] for row in f]
+	badcount = 0
 	for w in word_list:
-		w = w.strip()
-		counts.append([len(w), len(''.join([''.join(list(filter(lambda ch: ch in sonorants, x))) for x in w])), len(''.join([''.join(list(filter(lambda ch: ch not in vowels, x))) for x in w]))])
+		try:
+			w = g2p([w], delim=".")[0][1][0] #translate to IPA using g2p module
+		except TypeError:
+			# print(w, 'bad characters')
+			badcount += 1
+		else:
+			w = ''.join(list(filter(lambda ch: ch not in '.', w)))
+			counts.append([len(w), len(''.join(list(filter(lambda ch: ch in sonorants, w)))), len(''.join(list(filter(lambda ch: ch not in vowels, w))))])
 
-print('\nAverage sonorants per unique word:', sum([x[1] for x in counts]) / len(counts), '\n')
+print('number of words:', len(counts), '\n')
+print('number of word with formatting problems:', badcount, '\n')
+
+print('Average sonorants per unique word:', sum([x[1] for x in counts]) / len(counts), '\n')
 
 print('Average characters per unique word:', sum([x[0] for x in counts]) / len(counts), '\n')
 
@@ -37,11 +48,12 @@ with open(corpus) as f:
 		w = w.strip()
 		counts.append([len(w), len(''.join([''.join(list(filter(lambda ch: ch in sonorants, x))) for x in w])), len(''.join([''.join(list(filter(lambda ch: ch not in vowels, x))) for x in w]))])
 
+print('number of words:', len(counts), '\n')
+
 print('Average sonorants per word:', sum([x[1] for x in counts]) / len(counts), '\n')
 
 print('Average characters per word:', sum([x[0] for x in counts]) / len(counts), '\n')
 
 print('Average non-vowel characters per word:', sum([x[2] for x in counts]) / len(counts), '\n')
 
-print(g2p(['ánm'], delim = '.'))
 
